b50f13160697ac7ef3582461324b1a4d42e078f7
Ryan Shin <ryanminjaeshin@gmail.com>
2021-03-07 15:42:20 -0800

    Add temp/weather

--- src/App.js
+++ src/App.js
@@ -6,6 +6,19 @@ const api = {
 }
 
 function App() {
+
+  const dateBuilder = d => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
   return (
     <div className="app">
       <main>
@@ -14,8 +27,19 @@ function App() {
             type="text"
             className="search-bar"
             placeholder="Search..."
-          >
-          </input>
+          />
+          <div className="location-box">
+            <div className="location">New York City, US</div>
+            <div className="date">{dateBuilder(new Date())}</div>
+          </div>
+          <div className="weather-box">
+            <div className="temp">
+              15°
+            </div>
+            <div className="weather">
+              Sunny
+            </div>
+          </div>
         </div>
       </main>
     </div>
