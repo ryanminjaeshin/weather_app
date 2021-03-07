7feffd846fe492e2e693f89a46fcfb7d0fccbe7d
Ryan Shin <ryanminjaeshin@gmail.com>
2021-03-07 15:10:10 -0800

    Add weather API key

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import './App.css';
 
+const api = {
+  key: "c1162f70597c56244d2103c91d4b9dda",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
 function App() {
   return (
     <div className="App">
