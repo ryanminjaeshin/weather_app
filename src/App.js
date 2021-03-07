0336771bfd0a64894e20aed74172c3a54db3bf35
Ryan Shin <ryanminjaeshin@gmail.com>
2021-03-07 15:58:30 -0800

    Add search method

--- src/App.js
+++ src/App.js
@@ -11,6 +11,14 @@ function App() {
   const [query, setQuery] = useState('');
   const [weather, setWeather] = useState({});
 
+  const search = e => {
+    if (e.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&unites=metric&APPID=${api.key}`)
+      .then(res => res.json())
+      .then(result => setWeather(result));
+    }
+  }
+
   const dateBuilder = d => {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
