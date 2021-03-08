9de5695244d508a5e89713fed8ee9cd31807b58b
Ryan Shin <ryanminjaeshin@gmail.com>
2021-03-07 16:12:16 -0800

    Add ternary operator

--- src/App.js
+++ src/App.js
@@ -48,18 +48,22 @@ function App() {
             onKeyPress={search}
           />
         </div>
-          <div className="location-box">
-            <div className="location">{weather.name}, {weather.sys.country}</div>
-            <div className="date">{dateBuilder(new Date())}</div>
-          </div>
-          <div className="weather-box">
-            <div className="temp">
-              15°
+        {(typeof weather.main != "undefined") ? (
+          <div>
+            <div className="location-box">
+              <div className="location">{weather.name}, {weather.sys.country}</div>
+              <div className="date">{dateBuilder(new Date())}</div>
             </div>
-            <div className="weather">
-              Sunny
+            <div className="weather-box">
+              <div className="temp">
+                {Math.round(weather.main.temp)}°C
+              </div>
+              <div className="weather">
+                {weather.weather[0].main}
+              </div>
             </div>
           </div>
+        ) : ('')}
       </main>
     </div>
   );
